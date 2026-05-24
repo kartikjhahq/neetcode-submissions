3d160e78d898fde3425fef1a6726e864c8639a31
kartikjhahq <kartikjhahq@gmail.com>
2026-05-24 07:43:12 +0530

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    singleNumber(nums) {
+        let result = 0;
+
+        for (let i = 0; i < nums.length; i++) {
+            result ^= nums[i];
+        }
+        return result;
+    }
+}
